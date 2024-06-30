372c35a924ffc73665e3d8ef6f5d8af754ecbfbb
Pratap Pawar <87373605+pawarspeaks@users.noreply.github.com>
2024-07-01 00:05:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 import streamlit as st
-import webbrowser
 
 def main():
     st.title("Website Redirection")
 
-    st.write("Click the buttons below to redirect to respective websites:")
+    st.write("Click the links below to redirect to respective websites:")
 
-    if st.button("User Page"):
-        webbrowser.open_new_tab("https://hospital-management-user.streamlit.app/")
-
-    if st.button("Hospital Page"):
-        webbrowser.open_new_tab("https://smart-healthcare-assistant-prasunethon.streamlit.app/")
-
-    if st.button("Smart City"):
-        webbrowser.open_new_tab("https://smart-city-management.streamlit.app/")
+    st.markdown("[User Page](https://hospital-management-user.streamlit.app/)")
+    st.markdown("[Hospital Page](https://smart-healthcare-assistant-prasunethon.streamlit.app/)")
+    st.markdown("[Smart City](https://smart-city-management.streamlit.app/)")
 
 if __name__ == "__main__":
     main()
