7034c26e09c224b1b2a06e16f18471997629c528
Pratap Pawar <87373605+pawarspeaks@users.noreply.github.com>
2024-06-30 23:49:05 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import webbrowser
+
+def main():
+    st.title("Website Redirection")
+
+    st.write("Click the buttons below to redirect to respective websites:")
+
+    if st.button("User Page"):
+        webbrowser.open_new_tab("https://hospital-management-user.streamlit.app/")
+
+    if st.button("Hospital Page"):
+        webbrowser.open_new_tab("https://smart-healthcare-assistant-prasunethon.streamlit.app/")
+
+    if st.button("Smart City"):
+        webbrowser.open_new_tab("https://smart-city-management.streamlit.app/")
+
+if name == "main":
+    main()
